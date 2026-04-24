a9b68cacc6196384f0cf9c90f2b1d2cc841d0dbd
Brayyzzz05 <brayvenwong05@gmail.com>
2026-04-24 20:52:08 +0800

    Create rconHandler.js

---
+++ rconHandler.js
@@ -0,0 +1,22 @@
+import Rcon from "rcon-client";
+
+let rcon;
+
+export async function connectRcon() {
+  try {
+    rcon = await Rcon.Rcon.connect({
+      host: process.env.RCON_HOST,
+      port: Number(process.env.RCON_PORT),
+      password: process.env.RCON_PASSWORD
+    });
+
+    console.log("🎮 RCON connected");
+  } catch (err) {
+    console.log("⚠️ RCON offline, bot still running");
+  }
+}
+
+export async function runCommand(cmd) {
+  if (!rcon) throw new Error("RCON not connected");
+  return rcon.send(cmd);
+}
